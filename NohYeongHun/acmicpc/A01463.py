5f5a1605af945fd3f71af8dae3fd8ae20d60d18d
YeongHun <dkzndk1499@gmail.com>
2022-05-31 17:51:34 +0900

    A01463.py

---
+++ NohYeongHun/acmicpc/A01463.py
@@ -0,0 +1,20 @@
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+n = int(input())
+dp = [0] * (n + 1)
+
+dp[1] = 0
+for i in range(2, n + 1):
+    dp[i] = dp[i - 1] + 1
+
+    if i % 2 == 0:
+        val = dp[i // 2] + 1
+        dp[i] = min(dp[i], val)
+    if i % 3 == 0:
+        val = dp[i // 3] + 1
+        dp[i] = min(dp[i], val)
+    
+print(dp[n])
